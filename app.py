fac02a98ce882b3825f9a5318f71bdcd82614961
yehudise <yehudise02@gmail.com>
2024-06-16 09:07:52 -0500

    final

--- app.py
+++ app.py
@@ -1,21 +1,59 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, request, session, jsonify
 from boggle import Boggle
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "abcde1"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+debug = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
 
-
-@app.route("/boggle_board", methods = ['GET','POST'])
-def set_up_baord():
-    """Sets the Boggle Board."""
+@app.route('/')
+def generate_board():
     board = boggle_game.make_board()
-    return render_template('generateBoard.html', board = board)
+    session['board'] = board
+    highscore = session.get("nplays", 0)
+    nplays = session.get("nplays", 0)
 
+    return render_template('generateBoard.html', board = board, highscore=highscore, nplays=nplays)
+
+# @app.route("/boggle_board", methods = ['GET','POST'])
+# def set_up_board():
+#     """Sets the Boggle Board."""
     
+#     # if 'board' in session:
+#     #     board = session['board']
+#     # else:
+#     #     board = boggle_game.make_board()
+#     #     session['board'] = board
 
-ç
+#     if request.method == 'POST':
+#         guess = request.form.get('guess')
+#         session['guess'] = guess
+#         result = boggle_game.check_valid_word(board, guess)
+#         return render_template('generateBoard.html', board=board, result=result, last_guess=guess)
+@app.route("/check-word")
+def check_word():
+    word = request.args["word"]
+    board = session["board"]
+    response = boggle_game.check_valid_word(board, word)
 
+    return jsonify({'result': response})
 
+@app.route("/post_score", methods = ["POST"])
+def post_score():
+
+    score = request.json["score"]
+    highscore = session.get("highscore", 0)
+    nplays = session.get("nplays", 0)
+    session['nplays'] = nplays + 1
+    session['highscore'] = max(score, highscore)
+
+    return jsonify(brokeRecord = score > highscore)
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
--- test.py
+++ test.py
@@ -8,3 +8,48 @@ class FlaskTests(TestCase):
 
     # TODO -- write tests for every view function / feature!
 
+    def setUp(self):
+        """set up tests"""
+
+        self.client = app.test_client()
+        app.config['TESTING'] = True
+
+    def test_homepage(self):
+        """Make sure information is in the session and HTML is displayed"""
+
+        with self.client:
+            response = self.client.get('/')
+            self.assertIn('board', session)
+            self.assertIsNone(session.get('highscore'))
+            self.assertIsNone(session.get('nplays'))
+            self.assertIn(b'<p>High Score:', response.data)
+            self.assertIn(b'Score:', response.data)
+            self.assertIn(b'Seconds Left:', response.data)
+
+    def test_valid_word(self):
+        """Test if word is valid by modifying the board in the session"""
+
+        with self.client as client:
+            with client.session_transaction() as sess:
+                sess['board'] = [["D", "O", "G", "G", "G"], 
+                                 ["D", "O", "G", "G", "G"], 
+                                 ["D", "O", "G", "G", "G"], 
+                                 ["D", "O", "G", "G", "G"], 
+                                 ["D", "O", "G", "G", "G"]]
+        response = self.client.get('/check-word?word=dog')
+        self.assertEqual(response.json['result'], 'ok')
+
+    def test_invalid_word(self):
+        """Test if word is in the dictionary"""
+
+        self.client.get('/')
+        response = self.client.get('/check-word?word=impossible')
+        self.assertEqual(response.json['result'], 'not-on-board')
+
+    def non_english_word(self):
+        """Test if word is on the board"""
+
+        self.client.get('/')
+        response = self.client.get(
+            '/check-word?word=abhfisbdiueunundsndo')
+        self.assertEqual(response.json['result'], 'not-word')
